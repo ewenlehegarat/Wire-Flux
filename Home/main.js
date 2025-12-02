ffe20baabdb3fdae1a0cf861e22560cf75600428
Ewen Le Hegarat <ewenlehegaratpro@gmail.com>
2025-12-02 12:20:09 +0100

    update

--- Home/main.js
+++ Home/main.js
@@ -1,18 +1,32 @@
-const buttonGauche = document.querySelector('.button_gauche');
-const buttonDroite = document.querySelector('.button_droite');
+const buttonGauche = document.querySelector('.bouton_gauche')
+const buttonDroite = document.querySelector('.bouton_droite')
+const hiddenDiv = document.querySelectorAll('.hidden_div')
+const buttonPlay = document.querySelectorAll('.button_play')
 
-buttonGauche.addEventListener('mouseover', () => {
-    buttonGauche.src = url('/img/button_gauche_hover.png');
+buttonGauche.addEventListener('mouseover', function(){
+    buttonGauche.src = '/img/button_gauche_hover.png'
 })
 
-buttonGauche.addEventListener('mouseout', () => {
-    buttonGauche.src = url('/img/button_gauche.png');
+buttonGauche.addEventListener('mouseout', function(){
+    buttonGauche.src = '/img/bouton_gauche.png'
 })
 
-buttonDroite.addEventListener('mouseover', () => {
-    buttonDroite.src = url('/img/button_droite_hover.png');
+
+buttonDroite.addEventListener('mouseover', function(){
+    buttonDroite.src = '/img/button_droite_hover.png'
 })
 
-buttonDroite.addEventListener('mouseout', () => {
-    buttonDroite.src = url('/img/button_droite.png');
+buttonDroite.addEventListener('mouseout', function(){
+    buttonDroite.src = '/img/button_droite.png'
 })
+
+
+buttonPlay.forEach(buttonplay => {
+    buttonplay.addEventListener('mouseover', function(){
+        buttonplay.src = '/img/Button_play_hover.png'
+    })
+
+    buttonplay.addEventListener('mouseout', function(){
+        buttonplay.src = '/img/Button_play.png'
+    })
+});
