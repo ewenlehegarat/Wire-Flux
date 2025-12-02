89dbcccf740bb3ef1ae4921c1717debf77c3a5f4
Ewen Le Hegarat <ewenlehegaratpro@gmail.com>
2025-12-02 12:27:10 +0100

    p

--- main.js
+++ main.js
@@ -4,29 +4,29 @@ const hiddenDiv = document.querySelectorAll('.hidden_div')
 const buttonPlay = document.querySelectorAll('.button_play')
 
 buttonGauche.addEventListener('mouseover', function(){
-    buttonGauche.src = '/img/button_gauche_hover.png'
+    buttonGauche.src = 'img/button_gauche_hover.png'
 })
 
 buttonGauche.addEventListener('mouseout', function(){
-    buttonGauche.src = '/img/bouton_gauche.png'
+    buttonGauche.src = 'img/bouton_gauche.png'
 })
 
 
 buttonDroite.addEventListener('mouseover', function(){
-    buttonDroite.src = '/img/button_droite_hover.png'
+    buttonDroite.src = 'img/button_droite_hover.png'
 })
 
 buttonDroite.addEventListener('mouseout', function(){
-    buttonDroite.src = '/img/button_droite.png'
+    buttonDroite.src = 'img/button_droite.png'
 })
 
 
 buttonPlay.forEach(buttonplay => {
     buttonplay.addEventListener('mouseover', function(){
-        buttonplay.src = '/img/Button_play_hover.png'
+        buttonplay.src = 'img/Button_play_hover.png'
     })
 
     buttonplay.addEventListener('mouseout', function(){
-        buttonplay.src = '/img/Button_play.png'
+        buttonplay.src = 'img/Button_play.png'
     })
 });
